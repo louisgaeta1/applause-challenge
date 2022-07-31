9ce7628889528296ae7a87429f3fd691dabe421f
Louis Gaeta <lgaeta.zteam@zuora.com>
2022-07-31 17:52:49 -0400

    experience calculation

--- app/models/tester.rb
+++ app/models/tester.rb
@@ -2,4 +2,8 @@ class Tester < ApplicationRecord
   has_many :testers_devices, foreign_key: :testers_id, class_name: 'TestersDevice'
   has_many :devices, through: :testers_devices, source: :device
   has_many :bugs, foreign_key: :testers_id
+
+  def get_experience(device_ids)
+    self.bugs.where(devices_id: device_ids).count
+  end
 end
